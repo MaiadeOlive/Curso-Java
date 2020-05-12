dfdaec1a447f347eef2b3b420355bd2aabed5d08
Maia de Oliveira <fernandamaiadeoliveira@gmail.com>
2020-05-11 23:00:45 -0300

    exercicio 12 polimorfismo

---
+++ java-exercicio-12-polimorfismo/src/main/java/application/Program.java
@@ -0,0 +1,52 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.Employee;
+import entities.OutSorcedEmployee;
+
+public class Program {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		List<Employee> list = new ArrayList<Employee>();
+
+		System.out.print("Enter the number of employees: ");
+		Integer num = sc.nextInt();
+
+		for (int i = 1; i <= num; i++) {
+			System.out.print("Outsorced (y/n): ");
+			char ch = sc.next().charAt(0);
+
+			System.out.printf("Employee %d:%n ", i);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+
+			System.out.print("Hours: ");
+			Integer hour = sc.nextInt();
+
+			System.out.print("Value per Hour: ");
+			Double valuePerHour = sc.nextDouble();
+
+			if (ch == 'y') {
+				System.out.print("Additional Charge: ");
+				Double additionalCharge = sc.nextDouble();
+				list.add(new OutSorcedEmployee(name, hour,valuePerHour,additionalCharge));
+			}
+			else {
+				list.add(new Employee(name, hour,valuePerHour));
+			}
+		}
+		System.out.println();
+		System.out.println("Payments: ");
+
+		for (Employee emp : list ){
+			System.out.println(emp.getName() + " - $" + String.format("%.2f", emp.payment()));
+		}
+
+		sc.close();
+	}
+}
---
+++ java-exercicio-12-polimorfismo/src/main/java/entities/OutSorcedEmployee.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class OutSorcedEmployee extends Employee {
+
+	private Double additionalCharge;
+
+	public OutSorcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+		super(name, hours, valuePerHour);
+		this.additionalCharge = additionalCharge;
+	}
+
+	public Double getAdditionalCharge() {
+		return additionalCharge;
+	}
+
+	public void setAdditionalCharge(Double additionalCharge) {
+		this.additionalCharge = additionalCharge;
+	}
+
+	@Override
+	public Double payment(){
+		return super.payment() + additionalCharge * 1.1;
+	}
+}
