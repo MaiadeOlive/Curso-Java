95cd1ffae562947a2454b07e676ea88888ed51e2
Maia de Oliveira <fernandamaiadeoliveira@gmail.com>
2020-07-31 21:50:18 -0300

    exercicio nivelamento hibernate JPA

--- java-exercicio-20-JPA-hibernate/src/main/java/application/Program.java
+++ java-exercicio-20-JPA-hibernate/src/main/java/application/Program.java
@@ -1,18 +1,38 @@
 package application;
 
-import entities.People;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import domain.People;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		People p1 = new People(1, "Carlos da Silva", "carlos@gmail.com");
-		People p2 = new People(2, "Eliana Souto", "eliana@gmail.coma");
-		People p3 = new People(3, "Cynthia Lima", "cynthia@gmail.com");
+//		People p1 = new People(null, "Carlos da Silva", "carlos@gmail.com");
+//		People p2 = new People(null, "Eliana Souto", "eliana@gmail.coma");
+//		People p3 = new People(null, "Cynthia Lima", "cynthia@gmail.com");
+		People p4 = new People(null, "José Nelson", "nelson@gmil.com");
 
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
 
+		em.getTransaction().begin();
+//		em.persist(p1);
+//		em.persist(p2);
+//		em.persist(p3);
+		em.persist(p4);
+
+		People p = em.find(People.class, 4);
+		System.out.println("Pessoa: " + p);
+		em.remove(p);
+		em.getTransaction().commit();
+		System.out.println("Pessoa Removida!");
+
+		System.out.println("Pronto!");
+
+		em.close();
+		emf.close();
 	}
 }
--- java-exercicio-20-JPA-hibernate/src/main/java/domain/People.java
+++ java-exercicio-20-JPA-hibernate/src/main/java/domain/People.java
@@ -1,11 +1,24 @@
-package entities;
+package domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class People implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer ID;
 	private String name;
 	private String email;
+
+	public People(){
+	}
 
 	public People(Integer ID, String name, String email) {
 		this.ID = ID;
